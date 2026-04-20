a0113ba4b1d07b0b5a7da96660e26b8dac6b1871
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-20 14:03:41 -0400

    feat: add user schema and service, implement CORS with cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const whitelist = process.env.CORS_WHITELIST
+    ? process.env.CORS_WHITELIST.split(',')
+    : [];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Não permitido pelo CORS'));
+      }
+    },
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
