cdaf5786222e89ab5d7dd58a01ede37907a0119a
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-22 14:26:19 -0400

    refactor: simplify cookie configuration by using a constant for production check

--- src/modules/authentication/authentication.resolver.ts
+++ src/modules/authentication/authentication.resolver.ts
@@ -1,7 +1,10 @@
+import { Public } from '@/common/decorators/public.decorator';
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Response } from 'express';
 import { AuthenticationService } from './authentication.service';
-import { Request, Response } from 'express';
 import { LoginInput } from './dtos/login.input';
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 @Resolver()
 export class AuthenticationResolver {
@@ -12,6 +15,7 @@ export class AuthenticationResolver {
     return 'ok';
   }
 
+  @Public()
   @Mutation(() => Boolean)
   async login(
     @Args('input') input: LoginInput,
@@ -22,16 +26,16 @@ export class AuthenticationResolver {
 
     context.res.cookie('accessToken', accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: IS_PRODUCTION,
+      sameSite: IS_PRODUCTION ? 'none' : 'lax',
       maxAge: 15 * 60 * 1000, // 15 minutos
       path: '/',
     });
 
     context.res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: IS_PRODUCTION,
+      sameSite: IS_PRODUCTION ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
       path: '/',
     });
